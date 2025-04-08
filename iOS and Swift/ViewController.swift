1f6c0b63818e5ae1994d738790e0183ffb6992ea
Paulo Santos(Personal) <paulo.santos@compasso.com.br>
2025-04-08 17:05:29 -0300

    Initial Commit

---
+++ iOS and Swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS and Swift
+//
+//  Created by Paulo Afonso Della Mêa dos Santos on 08/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
