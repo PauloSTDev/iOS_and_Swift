685bd1c34b05c7e9dbbe48a14625c77dcc05f8f1
Paulo Santos(Personal) <paulo.santos@compasso.com.br>
2025-04-09 15:44:29 -0300

    feat(rollButton): Conecta UI de imagens e adiciona função do botão de rolagem de dados em viewController.swift

--- iOS and Swift/ViewController.swift
+++ iOS and Swift/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        // Dot notation
+        // Who             What     value
+        firstDiceImageView.image = UIImage(imageLiteralResourceName: "dice\(Int.random(in: 1...6))")
+        secondDiceImageView.image = UIImage(imageLiteralResourceName: "dice\(Int.random(in: 1...6))")
+    }
 }
 
