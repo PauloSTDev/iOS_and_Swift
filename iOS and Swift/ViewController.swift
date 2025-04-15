7abc7bb184aa62502c3b2c7b116dfc7d5acf273f
Paulo Santos(Personal) <paulo.santos@compasso.com.br>
2025-04-15 15:36:49 -0300

    feat(PlaySound): Adiciona AVFoundation e função para acionar teclas com som

--- iOS and Swift/ViewController.swift
+++ iOS and Swift/ViewController.swift
@@ -6,14 +6,37 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let delay : Double = 0.2
+        sender.alpha = 0.5
+        
+        print("Start")
+        playSound(soundName: sender.currentTitle!)
+        DispatchQueue.main.asyncAfter(deadline: .now() + delay) {
+            print("End")
+            sender.alpha = 1.0
+        }
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+        
+    }
+    
+    
+    
+    
 }
 
