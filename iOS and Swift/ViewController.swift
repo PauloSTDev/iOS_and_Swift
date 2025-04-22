c3689e21d6507b22e22bdab5791f56f99798b227
Paulo Santos(Personal) <paulo.santos@compasso.com.br>
2025-04-22 16:20:05 -0300

    feature(egg-timer): Adiciona progressBar e linka tittleLabel e progressbar no ViewController

--- iOS and Swift/ViewController.swift
+++ iOS and Swift/ViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var tittleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
     let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
     
     var secondsRemaining = 60
     
+    var timer = Timer()
+    
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
         let hardness = sender.currentTitle!
+        
+        timer.invalidate()
         
         secondsRemaining = eggTimes[hardness]!
         
-       Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+       timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
@@ -26,6 +34,10 @@ class ViewController: UIViewController {
             print("\(secondsRemaining) seconds.")
             secondsRemaining -= 1
             
+        }
+        else {
+            timer.invalidate()
+            tittleLabel.text = "Done!"
         }
         
     }
